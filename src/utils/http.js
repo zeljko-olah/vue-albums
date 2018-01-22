c635521670cafe4b6ac4352b06d8c2559778d5b7
zeljko-olah <olah.zeljko@gmail.com>
2018-01-22 17:46:24 +0100

    heroku config try 2

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 // create custom axios instance
 const http = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://vue-albums.herokuapp.com/api'
 })
 
 // export axios as vue plugin to use it as $http
